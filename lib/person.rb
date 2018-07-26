151001ddc6030f97af90fe1880be87afb170e7ff
Ashleigh Chun <ashleigh.chun@wework.com>
2018-07-26 04:35:10 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,14 @@
  class Person
-   initialize 
+   def initialize(name)
+     @name = name
+   end
+   
    def name=(person_name)
      @name=person_name
    end
    
+   def name
+     @name
+   end
+ end
+ 

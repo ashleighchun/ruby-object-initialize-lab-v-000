294e2a9edf294fdf62e47700b01e3e426ef39eda
Ashleigh Chun <ashleigh.chun@wework.com>
2018-07-26 04:50:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,16 @@
 class Dog
-   def initialize(name)
-     @name = name
-   end
+  def initialize(name)
+    @name = name
+  end
    
-   def name=(dog_name)
-     @name=dog_name
-   end
+  def name=(dog_name)
+    @name=dog_name
+  end
    
-   def name
-     @name
-   end
- end
+  def name
+    @name
+  end
+end
+ 
+ 
  
